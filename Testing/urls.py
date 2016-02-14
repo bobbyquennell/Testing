c053b5437a0cb8a594f56788c5496e50087aa599
Yang <yang.wang04@gmail.com>
2016-02-14 13:25:32 +1100

    create a new application named app01 under Testing project

--- Testing/urls.py
+++ Testing/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^app/', include('app.urls')),
+    url(r'^app01/', include('app01.urls')),
 
 ]
 
