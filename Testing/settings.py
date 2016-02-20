66e65db82b105dec6704093679e89384ca600bdf
Yang <yang.wang04@gmail.com>
2016-02-20 17:26:09 +1100

    Modified index.html with bootstrap path

--- Testing/settings.py
+++ Testing/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
@@ -131,6 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
+
 
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static")
 
